d6a8de502ff86160584027296098c8bb489e8dd6
Osvaldx <romanonahuel24@gmail.com>
2024-12-18 23:06:41 -0300

    modified function: spam_validacion

--- tiktok-assitant.py
+++ tiktok-assitant.py
@@ -22,24 +22,25 @@ ___        ___  __                    ___
 
 async def mensaje_spam_validacion(mensaje: str)-> bool:
     retorno = False
+    emojis = 0
     if(not mensaje.replace(" ","").isalpha()):
-        emojis = 0
         for letra in mensaje:
             if(ord(letra) > 255):
                 emojis += 1
         
         retorno = True if emojis >= 5 else False
     
-    contador = 0
-    auxiliar = ""
-    for i in range(len(mensaje)):
-        if(i == 0):
+    if(emojis <= 5):
+        contador = 0
+        auxiliar = ""
+        for i in range(len(mensaje)):
+            if(i == 0):
+                auxiliar = mensaje[i]
+            elif(mensaje[i] == auxiliar):
+                contador += 1
             auxiliar = mensaje[i]
-        elif(mensaje[i] == auxiliar):
-            contador += 1
-        auxiliar = mensaje[i]
-    
-    retorno = True if contador >= 5 else False
+        
+        retorno = True if contador >= 5 else False
     
     return retorno
 
